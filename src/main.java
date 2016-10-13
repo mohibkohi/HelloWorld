0f9284538f77bf7a457284bf5978a978c3fa3cb5
mokohi <mokohi@uw.edu>
2016-10-13 16:38:03 -0700

    2nd commit with the new message

--- src/main.java
+++ src/main.java
@@ -3,4 +3,7 @@ public class main {
 	public static void main(String args[]) {
 		System.out.println("message");
 	}
+	public void newMessage(){
+		System.out.println("this is the updated message");
+	}
 }
